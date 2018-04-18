2a173adc07061df32cafc7b1147dee8f5468c114
Goffert van Gool <ruphin@ruphin.net>
2018-04-18 12:45:17 +0200

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,108 @@
+import { GluonElement, html } from '../node_modules/gluonjs/gluon.js';
+
+class AppElement extends GluonElement {
+
+  get template() {
+    return html`
+      <link rel="stylesheet" href="/bootstrap.min.css">
+      <div class="container">
+        <div class="row">
+      
+          <div class="col-md-6 offset-md-3" style="margin-top: 2px">
+      
+            <div class="card">
+              <div class="card-header">
+                Login app
+              </div>
+      
+              <div class="card-body">
+                <div id="alert" class="alert" role="alert"></div>
+      
+                <form>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input id="username" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                  </div>
+      
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input id="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>
+      
+                  <button id="button" type="button" class="btn btn-primary">Submit</button>
+      
+                </form>
+      
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.$.button.addEventListener('click', (e) => this.buttonClickHandler(e))
+    this.requestStatus();
+  }
+
+  requestStatus() {
+    fetch('http://localhost:3000/api/status')
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          console.log("ERR")
+        }
+      })
+      .then(alert => this.setAlert(alert.text, alert.code))
+  }
+
+  buttonClickHandler(e) {
+
+    const username = this.$.username.value;
+    const password = this.$.password.value;
+
+    fetch('http://localhost:3000/api/login', {
+      body: JSON.stringify({ username, password }), // must match 'Content-Type' header
+      headers: {
+        'content-type': 'application/json'
+      },
+      method: 'POST'
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          console.log("ERR")
+        }
+      })
+      .then(alert => this.setAlert(alert.text, alert.code))
+  }
+
+  setAlert(text, code) {
+    if (code === 'red') {
+      this.$.alert.classList.remove("alert-success");
+      this.$.alert.classList.remove("alert-primary");
+      this.$.alert.classList.add("alert-danger");
+    }
+
+    if (code === 'green') {
+      this.$.alert.classList.remove("alert-danger");
+      this.$.alert.classList.remove("alert-primary");
+      this.$.alert.classList.add("alert-success");
+    }
+
+    if (code === 'blue') {
+      this.$.alert.classList.remove("alert-success");
+      this.$.alert.classList.remove("alert-danger");
+      this.$.alert.classList.add("alert-primary");
+    }
+
+    this.$.alert.innerText = text
+  }
+}
+
+customElements.define(AppElement.is, AppElement);
